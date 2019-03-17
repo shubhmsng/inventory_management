45f6b79ba8c65daf651b67ae17f302d85295685c
shubhmsng <shubhmsing@gmail.com>
2019-03-17 10:13:12 -0700

    cors access issue resolved

--- api/config.php
+++ api/config.php
@@ -1,6 +1,7 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 $method = $_SERVER['REQUEST_METHOD'];
 $data   = json_decode(file_get_contents('php://input'), true);
 
